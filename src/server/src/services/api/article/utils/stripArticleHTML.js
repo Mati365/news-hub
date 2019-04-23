8739442ca0324eb7bf65455e3e89257c094d9e1e
Mateusz Baginski <mateusz.baginski@firma.interia.pl>
2019-04-23 22:56:40 +0200

    add whitelist

--- src/server/src/services/api/article/utils/stripArticleHTML.js
+++ src/server/src/services/api/article/utils/stripArticleHTML.js
@@ -1,10 +1,17 @@
 import * as R from 'ramda';
 import striptags from 'striptags';
 
+const ALLOWED_TAGS = [
+  'a', 'strong', 'p', 'b', 'br', 'img',
+  'table', 'tr', 'th', 'td', 'hr',
+];
+
+const safeTags = html => striptags(html, ALLOWED_TAGS);
+
 const stripArticleHTML = R.evolve(
   {
-    lead: striptags,
-    content: striptags,
+    lead: safeTags,
+    content: safeTags,
   },
 );
 
