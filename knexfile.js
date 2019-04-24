3e94512d4d228e308bd953c87c494290e8450940
Mateusz Baginski <mateusz.baginski@firma.interia.pl>
2019-04-24 20:58:12 +0200

    fix env config

--- knexfile.js
+++ knexfile.js
@@ -6,27 +6,30 @@ const {env} = process;
 
 const joinDatabasePath = dbPath => path.resolve(`./src/server/src/db/${dbPath}`);
 
-module.exports = {
-  development: {
-    client: env.DB_ADAPTER || 'postgresql',
-    connection: env.DATABASE_URL || {
-      // if url not provided
-      port: env.DB_PORT,
-      host: env.DB_HOST,
-      database: env.DB_NAME,
-      user: env.DB_USER,
-      password: env.DB_PASSWORD,
-    },
-    ssl: true,
-    pool: {
-      min: 5,
-      max: 10,
-    },
-    migrations: {
-      directory: joinDatabasePath('migrations'),
-    },
-    seeds: {
-      directory: joinDatabasePath('seeds'),
-    },
+const ENV_CONFIG = {
+  client: env.DB_ADAPTER || 'postgresql',
+  connection: env.DATABASE_URL || {
+    // if url not provided
+    port: env.DB_PORT,
+    host: env.DB_HOST,
+    database: env.DB_NAME,
+    user: env.DB_USER,
+    password: env.DB_PASSWORD,
   },
+  ssl: true,
+  pool: {
+    min: 5,
+    max: 10,
+  },
+  migrations: {
+    directory: joinDatabasePath('migrations'),
+  },
+  seeds: {
+    directory: joinDatabasePath('seeds'),
+  },
+};
+
+module.exports = {
+  development: ENV_CONFIG,
+  production: ENV_CONFIG,
 };
