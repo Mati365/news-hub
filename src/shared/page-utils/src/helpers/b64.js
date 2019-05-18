774b3e12024cc57f20dff8f13c38fa665a6d7132
Mateusz Baginski <mateusz.baginski@firma.interia.pl>
2019-05-18 14:41:46 +0200

    @page/search: encoder b64 fixes

--- src/shared/page-utils/src/helpers/b64.js
+++ src/shared/page-utils/src/helpers/b64.js
@@ -1,11 +1,11 @@
 export const decodeB64 = str => (
   typeof atob === 'undefined'
     ? Buffer.from(str, 'base64').toString('binary')
-    : atob(str)
+    : decodeURIComponent(escape(atob(str)))
 );
 
 export const encodeB64 = str => (
   typeof btoa === 'undefined'
     ? Buffer.from(str, 'binary').toString('base64')
-    : btoa(str)
+    : btoa(unescape(encodeURIComponent(str)))
 );
